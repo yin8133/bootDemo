f670b4819c7ab982ae30e24873997fea9da2401a
yin <331393451@qq.com>
2018-09-19 10:04:03 +0800

    no message

--- springBoot-base/src/main/java/com/backstage/base/service/impl/UserServiceImpl.java
+++ springBoot-base/src/main/java/com/backstage/base/service/impl/UserServiceImpl.java
@@ -2,20 +2,13 @@ package com.backstage.base.service.impl;
 
 import com.backstage.base.mapper.UserMapper;
 import com.backstage.base.models.User;
-import com.backstage.base.service.IRedisService;
 import com.backstage.base.service.UserService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+
 
 /**
  * @author jiangcaijun
